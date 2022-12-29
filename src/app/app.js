c417ac72933588a8b50900e1278540cfa42a2c9a
DSoft <108980710+dhrubajyotifounderofDSoft@users.noreply.github.com>
2022-12-29 18:44:59 +0530

    Add files via upload

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,19 @@
 const author = "Dhrubajyoti Biswas"
-
-// Navigation {header}
-document.getElementsByClassName(options-text-p)
-document.getElementsByClassName(sidebar_icons_look)
-sidebar_icons_look onclick()=>{
+const file_explorer_btn = document.querySelector("#file_explorer");
+const sidebar_menu = document.querySelector(".sidebar-menu");
+function sidebar_menu_toggle() {
+    function sidebar_menu_toggle_flex(){
+        file_explorer_btn.addEventListener('click', () => {
+            sidebar_menu.style = ("display:flex")
+        })
+    }
+    file_explorer_btn.addEventListener('click', () => {
+        sidebar_menu.style = ("display:none")
+    })
     
+   if (file_explorer_btn.style.display == "none"){
+      dropdown.style.display = 'block';
+    } else {
+      dropdown.style.display = "none";
+    }
 }
