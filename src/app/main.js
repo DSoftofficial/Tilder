00afd2b142d772e0cdcd62135a6af2516d1849d9
DSoft <108980710+dhrubajyotifounderofDSoft@users.noreply.github.com>
2023-01-04 17:07:58 +0530

    Added Shell files(start)

--- src/app/main.js
+++ src/app/main.js
@@ -1,42 +1,42 @@
-const { app, BrowserWindow } = require('electron')
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+// const { app, BrowserWindow } = require('electron')
+// const createWindow = () => {
+//   const win = new BrowserWindow({
+//     width: 800,
+//     height: 600
+//   })
 
-  win.loadFile('app.html')
-}
-app.whenReady().then(() => {
-  createWindow()
-})
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
-app.whenReady().then(() => {
-  createWindow()
+//   win.loadFile('app.html')
+// }
+// app.whenReady().then(() => {
+//   createWindow()
+// })
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') app.quit()
+// })
+// app.whenReady().then(() => {
+//   createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-// const author = "Dhrubajyoti Biswas"
-const file_explorer_btn = document.querySelector("#file_explorer");
-const sidebar_menu = document.querySelector(".sidebar-menu");
-function sidebar_menu_toggle() {
-  function sidebar_menu_toggle_flex() {
-    sidebar_menu.style = ("display:flex")
-  }
-  function sidebar_menu_toggle_none() {
-    sidebar_menu.style = ("display:none")
-  }
-  if (file_explorer_btn.style.display == "none") {
-    sidebar_menu_toggle_flex()
-  } else {
-    sidebar_menu_toggle_none()
-  }
-  file_explorer_btn.addEventListener('click', ()=>{
+//   app.on('activate', () => {
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+//   })
+// })
+// // const author = "Dhrubajyoti Biswas"
+// const file_explorer_btn = document.querySelector("#file_explorer");
+// const sidebar_menu = document.querySelector(".sidebar-menu");
+// function sidebar_menu_toggle() {
+//   function sidebar_menu_toggle_flex() {
+//     sidebar_menu.style = ("display:flex")
+//   }
+//   function sidebar_menu_toggle_none() {
+//     sidebar_menu.style = ("display:none")
+//   }
+//   if (file_explorer_btn.style.display == "none") {
+//     sidebar_menu_toggle_flex()
+//   } else {
+//     sidebar_menu_toggle_none()
+//   }
+//   file_explorer_btn.addEventListener('click', ()=>{
     
-  })
-}
+//   })
+// }
 console.log("Hello World")
