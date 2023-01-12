e73042047cacdbf121ca9706f8c31e9ba4bb9673
Dhrubajyoti Biswas <108980710+dhrubajyotifounderofDSoft@users.noreply.github.com>
2023-01-12 13:16:40 +0530

    Add files via upload

--- src/app/preload.js
+++ src/app/preload.js
@@ -1,7 +1,10 @@
-app.whenReady().then(() => {
-    createWindow()
-  
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  })
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
