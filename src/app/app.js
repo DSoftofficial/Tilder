3bb157cc0a308a929f1aa4099908ddb41cc024a6
Dhrubajyoti Biswas <108980710+dhrubajyotifounderofDSoft@users.noreply.github.com>
2023-01-15 18:10:58 +0530

    Add files via upload

--- src/app/app.js
+++ src/app/app.js
@@ -1,19 +1,6 @@
 const author = "Dhrubajyoti Biswas"
 const file_explorer_btn = document.querySelector("#file_explorer");
 const sidebar_menu = document.querySelector(".sidebar-menu");
-function sidebar_menu_toggle() {
-    function sidebar_menu_toggle_flex(){
-        file_explorer_btn.addEventListener('click', () => {
-            sidebar_menu.style = ("display:flex")
-        })
-    }
-    file_explorer_btn.addEventListener('click', () => {
-        sidebar_menu.style = ("display:none")
-    })
-    
-   if (file_explorer_btn.style.display == "none"){
-      dropdown.style.display = 'block';
-    } else {
-      dropdown.style.display = "none";
-    }
-}
+file_explorer_btn.addEventListener('click', () => {
+    sidebar_menu.style("display:flex")
+})
