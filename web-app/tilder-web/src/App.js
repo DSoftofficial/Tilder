d0d808a31d77b32b366a5f7a8b1e7360062099e4
Dhrubajyoti Biswas <108980710+dhrubajyotifounderofDSoft@users.noreply.github.com>
2023-04-01 21:53:09 +0530

    Updated `App.js`
    Co-Authored-By: Subrata Biswas <98461651+Dhrubajyo@users.noreply.github.com>
    Co-Authored-By: Moumita Biswas <122099970+Moumitaengineer@users.noreply.github.com>

--- web-app/tilder-web/src/App.js
+++ web-app/tilder-web/src/App.js
@@ -1,21 +1,25 @@
-import './App.css';
+import './css/App.css';
+import MenuBar from './components/MenuBar';
+import FileExplorerSideBar from './components/SideBar/FileExplorerSideBar';
+import PropTypes from 'prop-types'
+import SearchSideBar from './components/SideBar/SearchSideBar'
+import './css/MenuBar.css'
+import ReactDOM from "react-dom";
+import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
 
 function App() {
   return (
     <>
-      <nav>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </nav>
-    <navBar/>
+      <MenuBar />
+      <FileExplorerSideBar />
+      <SearchSideBar />
+      <Editor
+        height="90vh"
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+      />
     </>
-  );
+  )
 }
 
 export default App;
